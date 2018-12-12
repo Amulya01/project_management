e0179bb29deefe46a293582232cfbf568212d1fb
Amulya01 <36091233+Amulya01@users.noreply.github.com>
2018-12-12 18:29:15 +0530

    router protection

--- src/router.js
+++ src/router.js
@@ -8,9 +8,11 @@ import login from "./views/login.vue";
 import signup from "./views/signup.vue";
 import landing from "./components/Landing";
 import Home from "./components/Home.vue";
+import firebase from "firebase";
+
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   mode: "history",
   routes: [
     {
@@ -55,3 +57,14 @@ export default new Router({
     }
   ]
 });
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+  const isAuthenticated = firebase.auth().currentUser;
+  if (requiresAuth && !isAuthenticated) {
+    //console.log("not authenticated");
+    next("/login");
+  } else {
+    next();
+  }
+});
+export default router;
